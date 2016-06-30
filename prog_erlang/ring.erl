af988f70cadc5ceb8611e9a4e120b120faea095d
niftynei <niftynei@gmail.com>
2016-06-30 12:39:28 -0400

    a message passing ring that works

--- prog_erlang/ring.erl
+++ prog_erlang/ring.erl
@@ -1,29 +1,62 @@
 -module(ring).
 -compile(export_all).
 
-% create N processes in a ring
+start() ->
+  RootPid = spawn(fun() -> head_loop(self()) end),
+  _ChildPid = spawn_next(RootPid, RootPid),
+  _ChildPid ! {hello},
+  RootPid ! {child, _ChildPid},
+  RootPid.
 
-start(RootPid) ->
-  spawn(fun() -> loop({RootPid, self()}) end).
+spawn_next(RootPid, ParentPid) ->
+  spawn(fun() -> loop({RootPid, ParentPid}) end).
 
-%rpc(Pid, Request) ->
-%  Pid ! {self(), Request},
-%  receive
-%    {Pid, Response} ->
-%      Response
-%  end.
+head_loop(Pid) ->
+  receive
+    {child, _Pid} ->
+      io:format("Child Pid is ~p~n", [_Pid]),
+      head_loop(_Pid); 
+    {start, N} ->
+      statistics(runtime),
+      statistics(wall_clock),
+      Pid ! {ring, N},
+      head_loop(Pid);
+    {send, M} ->
+      Pid ! {message, M},
+      head_loop(Pid); 
+    {message, 0} ->
+      % print out how long it took
+      {_, Time1} = statistics(runtime),
+      {_, Time2} = statistics(wall_clock),
+      U1 = Time1 / 1000,
+      U2 = Time2 / 1000,
+      io:format("finished looping in ~ps (~ps)~n",[U1, U2]),
+      head_loop(Pid);
+    {message, M} ->
+      Pid ! {message, M - 1},
+      head_loop(Pid); 
+    {done, _Pid} ->
+      io:format("Ran out of Ns at ~p~n", [_Pid]),
+      head_loop(_Pid);
+    Any ->
+      io:format("~p Received:~p~n", [self(), Any]),
+      head_loop(Pid)
+   end.
 
 loop({RootPid, Pid}) ->
   receive
     {ring, 0} ->
-      io:format("Ran out of Ns at ~p~n", [self()]),
       RootPid ! {done, self()},
       loop({RootPid, Pid});
     {ring, N} ->
-      _Pid = start(RootPid),
+      _Pid = spawn_next(RootPid, self()),
       io:format("~p started new process ~p~n", [self(), _Pid]),
       _Pid ! {ring, N-1},
       loop({RootPid, Pid});
+    {message, M} ->
+      %io:format("~p relaying message number ~p~n", [self(), M]),
+      Pid ! {message, M},
+      loop({RootPid, Pid});      
     Any ->
       io:format("~p Received:~p~n", [self(), Any]),
       RootPid ! {ok},
